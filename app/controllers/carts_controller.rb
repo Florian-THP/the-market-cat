35984d8e9e7d4dd46efff670be1f1591b6a4a688
Florian-THP <triboutflorian@outlook.com>
2024-11-27 18:26:34 +0100

    partial turbo frame link

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,28 +9,28 @@ class CartsController < ApplicationController
   end
 
   def add_article_to_cart
-    article = Article.find(params[:article_id])  # Récupérer l'article par son ID
+    article = Article.find(params[:article_id]) 
     @cart = current_user.cart || current_user.create_cart
     @cart_item = @cart.cart_items.find_or_create_by(article: article)
-    @cart_item.increment(:quantity)  # Ajouter 1 à la quantité
+    @cart_item.increment(:quantity) 
     @cart_item.save
   
-    redirect_to cart_path, notice: "L'article a été ajouté au panier."
+ 
   end
   
 
   def remove_article_from_cart
-    article = Article.find(params[:article_id])  # Récupère l'article par son ID
+    article = Article.find(params[:article_id]) 
     @cart = current_user.cart
     cart_item = @cart.cart_items.find_by(article: article)
 
     if cart_item
       if cart_item.quantity > 1
-        cart_item.decrement(:quantity)  # Décrémente la quantité
+        cart_item.decrement(:quantity)
         cart_item.save
         flash[:notice] = "La quantité de l'article a été réduite."
       else
-        cart_item.destroy  # Si la quantité est 1, on supprime l'article du panier
+        cart_item.destroy 
         flash[:notice] = "L'article a été supprimé du panier."
       end
     else
@@ -42,7 +42,7 @@ class CartsController < ApplicationController
 
   def clear_cart
     @cart = current_user.cart
-    @cart.cart_items.destroy_all  # Supprime tous les articles du panier
+    @cart.cart_items.destroy_all 
     flash[:notice] = "Votre panier a été vidé."
     redirect_to cart_path
   end
