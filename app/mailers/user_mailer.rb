e45e62508f02b3dabeba1ea4bad8ce7125b9bb60
Florian-THP <triboutflorian@outlook.com>
2024-11-28 17:47:46 +0100

    add

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,21 @@
 class UserMailer < ApplicationMailer
 
   def order_confirmation(user, order)
-    @user = user 
+    @user = user
     @order = order
-    @articles = order.order_items.map(&:article)
-    @url = 'http://TheMarketCat.fr' 
+    @articles = order.order_items.map(&:article) # Liste des articles associés à la commande
+    @url = 'http://TheMarketCat.fr'
 
+    @articles.each do |article|
+      if article.avatar.attached?
+        attachments.inline["#{article.name}_avatar.jpg"] = article.avatar.download
+      end
+    end
 
+    # Envoi du mail
     mail(
       to: @user.email,
       subject: "Merci pour votre achat, #{@user.email} !"
     )
-
   end
 end
